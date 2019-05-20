70873026906fb5edfc5cd910def646bfa38bcf90
unknown <1403192518@qq.com>
2019-05-20 22:36:56 +0800

    feat: 添加Icon组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Button from './button'
+import Icon from './icon'
 
-ReactDOM.render(<Button></Button>, document.body)
+ReactDOM.render(<div>
+    <Icon name="wechat" />
+    <Icon name="alipay" />
+    <Icon name="qq" />
+</div>, document.getElementById('root'))
