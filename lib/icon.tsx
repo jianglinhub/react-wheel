6de048e4e99f222b919595b4715f98673713fa1d
unknown <1403192518@qq.com>
2019-05-22 00:12:53 +0800

    feat: 使用参数解构获取props

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -7,14 +7,13 @@ interface IconProps extends React.SVGAttributes<SVGElement> {
 	name: string
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-	const { className = '', ...restProps } = props
-
-	return (
-		<svg className={classes('fui-icon', className)} {...restProps}>
-			<use xlinkHref={`#${props.name}`}></use>
-		</svg>
-	)
-}
+const Icon: React.FunctionComponent<IconProps> = 
+	({ className, name, ...restProps }) => {
+		return (
+			<svg className={classes('fui-icon', className)} {...restProps}>
+				<use xlinkHref={`#${name}`}></use>
+			</svg>
+		)
+	}
 
 export default Icon
