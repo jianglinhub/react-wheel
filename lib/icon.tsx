ea4e8336dd8634e6f23717a1566a900248b3f2bc
unknown <1403192518@qq.com>
2019-05-21 23:34:30 +0800

    feat: onclick 类型添加

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,17 +3,18 @@ import './importIcons'
 import './icon.scss'
 
 interface IconProps {
-    name: string
+	name: string,
+	onClick: React.MouseEventHandler<SVGElement>
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props)=> {
-    return (
-        <span>
-            <svg className="fui-icon">
-                <use xlinkHref={`#${props.name}`}></use>
-            </svg>
-        </span>
-    )
+const Icon: React.FunctionComponent<IconProps> = (props) => {
+	return (
+		<span>
+			<svg className="fui-icon" onClick={props.onClick}>
+				<use xlinkHref={`#${props.name}`}></use>
+			</svg>
+		</span>
+	)
 }
 
 export default Icon
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Icon from './icon'
 
+const fn: React.MouseEventHandler = (e) => {
+	console.log(e.target)
+}
+
 ReactDOM.render(<div>
-    <Icon name="wechat" />
-    <Icon name="alipay" />
-    <Icon name="qq" />
+	<Icon name="qq" onClick={fn} />
 </div>, document.getElementById('root'))
