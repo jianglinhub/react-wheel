12c5e292c04bfe25aab872aded815ab89373664b
unknown <1403192518@qq.com>
2019-05-22 23:08:19 +0800

    feat: 添加jest-junit

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     verbose: true,
     clearMocks: false,
-    reporters: ["default",],
+    reporters: ["default", "jest-junit"],
 
     collectCoverage: true,
     collectCoverageFrom: ["lib/**/*.{ts,tsx}","!**/node_modules/**"],
