5e60eb98ceff233f9f5dc2604cf0953c6a817c93
unknown <1403192518@qq.com>
2019-05-22 22:03:20 +0800

    feat: 使用jest.fn()测试点击事件

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Icon from '../icon'
+import { mount } from 'enzyme'
 
 describe('Icon', () => {
-    it('is a react component', () => {
-        const json = renderer.create(<Icon name="alipay" />).toJSON()
-        expect(json).toMatchSnapshot()
-    })
+	it('render success', () => {
+		const json = renderer.create(<Icon name="alipay" />).toJSON()
+		expect(json).toMatchSnapshot()
+	})
+	it('onClick', () => {
+		const fn = jest.fn()
+		const component = mount(<Icon name="alipay" onClick={fn} />)
+		component.find('svg').simulate('click')
+		expect(fn).toBeCalled()
+	})
 })
