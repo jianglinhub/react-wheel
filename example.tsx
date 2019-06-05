596d17b5d766b6180a021eb5519829597923293a
unknown <1403192518@qq.com>
2019-06-05 23:38:35 +0800

    feat: 添加示例代码

--- example.tsx
+++ example.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router, Link, Route } from 'react-router-dom'
-import IconExample from './lib/icon/icon.example'
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom'
 import ButtonExample from './lib/button/button.example'
 import DialogExample from './lib/dialog/dialog.example'
 import LayoutExample from './lib/layout/layout.example'
 import { Layout, Aside, Content, Header, Footer } from './lib/layout/layout';
 import './example.scss'
+import IconDemo from './lib/icon/icon.demo'
 
 const logo = require('./logo.png')
 
@@ -24,21 +24,18 @@ ReactDOM.render((
           <h2>组件</h2>
           <ul>
             <li>
-              <Link to="/icon">Icon</Link>
+              <NavLink to="/icon">Icon</NavLink>
             </li>
             <li>
-              <Link to="/button">Button</Link>
+              <NavLink to="/dialog">Dialog对话框</NavLink>
             </li>
             <li>
-              <Link to="/dialog">Dialog对话框</Link>
-            </li>
-            <li>
-              <Link to="/layout">Layout</Link>
+              <NavLink to="/layout">Layout</NavLink>
             </li>
           </ul>
         </Aside>
         <Content className="site-main">
-          <Route path="/icon" component={IconExample} />
+          <Route path="/icon" component={IconDemo} />
           <Route path="/button" component={ButtonExample} />
           <Route path="/dialog" component={DialogExample} />
           <Route path="/layout" component={LayoutExample} />
